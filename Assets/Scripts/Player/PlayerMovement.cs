ea33bd0ec82b6153b0a33ec16df8984e137677bd
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-05 17:23:08 +0900

    create PlayerMovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour, IPlayerMovement
+{
+    [SerializeField]
+    private float m_speed;
+    private Rigidbody m_rigid;
+
+    private void Awake()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 inputVec)
+    {
+        Vector3 nextVec = inputVec * m_speed * Time.deltaTime;
+        m_rigid.MovePosition(nextVec);
+    }
+
+    public void MoveTo(Vector3 des)
+    {
+        // TODO
+    }
+
+    public void LookAtMouse(Vector3 inputMousePos)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(inputMousePos);
+        Vector3 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
