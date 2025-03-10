2ffca3f8e01ebaac50b6d919309728f22c9b7f87
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-10 18:06:25 +0900

    create ChangeCursor.cs

---
+++ Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D m_cursorImg;
+
+    void Start()
+    {
+        Cursor.SetCursor(m_cursorImg, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
