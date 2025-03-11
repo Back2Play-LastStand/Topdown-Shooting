508c9180b95ce59bd309c6bef6a06ab7c8426b6a
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-11 12:13:58 +0900

    create Player Aim

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private PlayerInput m_playerInput;
     private PlayerMovement m_playerMovement;
+    [SerializeField] private PlayerAim m_playerAim;
 
     void Awake()
     {
@@ -18,5 +19,6 @@ public class Player : MonoBehaviour
         m_playerMovement.Move(m_playerInput.InputVec);
         m_playerMovement.LookAtMouse(m_playerInput.MousePos);
         m_playerMovement.PlayAnim(m_playerInput.InputVec);
+        m_playerAim.AimTowardsMouse(m_playerInput.MousePos);
     }
 }
---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField] private Transform m_aimCamera;
+    [SerializeField] private float m_minCameraDistance = 1.5f;
+    [SerializeField] private float m_maxCameraDistance = 4f;
+
+    public void AimTowardsMouse(Vector2 inputMousePos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(inputMousePos);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity))
+        {
+            Vector3 dirAimPos = (hitInfo.point - transform.position).normalized;
+            float disPlayerToTarget = Vector3.Distance(transform.position, hitInfo.point);
+            float disAimCamera = Mathf.Clamp(disPlayerToTarget, m_minCameraDistance, m_maxCameraDistance);
+            Vector3 aimCameraPos = transform.position + dirAimPos * disAimCamera;
+            m_aimCamera.position = aimCameraPos;
+        }
+    }
+}
