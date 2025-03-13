f09b4108f6419fb264f19efe3c0d8e05ab5b790f
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-13 20:38:45 +0900

    create ServerSession.cs

---
+++ Assets/Scripts/Packet/ServerSession.cs
@@ -0,0 +1,51 @@
+using Google.Protobuf;
+using Protocol;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace ServerCore
+{
+
+    public class ServerSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            if (endPoint == null)
+                return;
+
+            Debug.Log($"OnConnected : {endPoint}");
+        }
+
+        public void Send(IMessage packet, int id)
+        {
+            ushort size = (ushort)packet.CalculateSize();
+            byte[] sendBuffer = new byte[size + 4];
+            Array.Copy(BitConverter.GetBytes((ushort)(size + 4)), 0, sendBuffer, 0, sizeof(ushort));
+            Array.Copy(BitConverter.GetBytes((ushort)id), 0, sendBuffer, 2, sizeof(ushort));
+            Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
+            Debug.Log($"send packet : {BitConverter.ToString(sendBuffer)}");
+            Send(new ArraySegment<byte>(sendBuffer));
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Debug.Log($"OnDisConnected : {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            Session session = this;
+
+            var protocolId = BitConverter.ToUInt16(session._recvArgs.Buffer, 2);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Debug.Log($"SendPacket : {numOfBytes}");
+        }
+    }
+}
