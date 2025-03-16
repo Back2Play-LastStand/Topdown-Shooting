0c16d26f3f736d80063d4b66985a864cdc7ecb12
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-17 08:42:20 +0900

    create ReqEnterHandler

--- Assets/Scripts/Packet/PacketHandler.cs
+++ Assets/Scripts/Packet/PacketHandler.cs
@@ -1,8 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Google.Protobuf;
+using Protocol;
+using ServerCore;
 
 public class PacketHandler
 {
-
+    public static void ReqEnterHandler(PacketSession session, IMessage packet)
+    {
+        REQ_ENTER enterPacket = packet as REQ_ENTER;
+        ServerSession serverSession = session as ServerSession;
+    }
 }
