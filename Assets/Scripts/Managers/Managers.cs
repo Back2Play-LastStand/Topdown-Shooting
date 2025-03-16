c30c77434b9eab313f74837f1bf5b08859f5995c
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-03-16 11:47:11 +0900

    create PacketManager

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -9,8 +9,10 @@ public class Managers : MonoBehaviour
 
     #region Contents
     NetworkManager _network = new();
+    PacketManager _packet = new();
 
     public static NetworkManager Network { get { return Instance._network; } }
+    public static PacketManager Packet { get {  return Instance._packet; } }
     #endregion
 
     public static void Init()
