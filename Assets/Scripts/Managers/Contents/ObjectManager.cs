579a8a39fa5a4b0578053fb7142c23ae98d3596d
hyuli0520 <minhyeogjeon945@gmail.com>
2025-03-24 12:13:00 +0900

    update object manager
    add object

--- Assets/Scripts/Managers/Contents/ObjectManager.cs
+++ Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -1,3 +1,4 @@
+using Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,28 @@ public class ObjectManager
     public MyPlayer MyPlayer { get; set; }
     Dictionary<ulong, GameObject> _objects = new Dictionary<ulong, GameObject>();
 
-    public void Add(int id, GameObject go)
+    public void Add(ObjectInfo info, bool myPlayer = false)
+    {
+        if (myPlayer)
+        {
+            GameObject go = Managers.Resource.Instantiate("MyPlayer");
+            go.name = info.Name;
+            _objects.Add(info.ObjectId, go);
+
+            MyPlayer = go.GetComponent<MyPlayer>();
+            MyPlayer.Id = info.ObjectId;
+        }
+        else
+        {
+            GameObject go = Managers.Resource.Instantiate("Player");
+            go.name = info.Name;
+            _objects.Add(info.ObjectId, go);
+
+            Player player = go.GetComponent<Player>();
+            player.Id = info.ObjectId;
+        }
+    }
+    public void Add(ulong id, GameObject go)
     {
         _objects.Add(id, go);
     }
