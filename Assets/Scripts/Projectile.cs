f2f7b826c94e8e8b51e0e168c2f77ec0e104e081
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-03 20:09:14 +0900

    update Projectile
    destory object

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float m_speed = 10;
+    private float m_lifeTime = 5f;
 
     public void SetSpeed(float speed)
     {
         m_speed = speed;
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, m_lifeTime);
     }
 
     void Update()
