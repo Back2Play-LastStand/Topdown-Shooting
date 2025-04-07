150c27dda44081be91f424b27136f5c0d51eb949
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-07 21:07:07 +0900

    update UIManager
    Show/Close PopupUI

--- Assets/Scripts/Managers/Core/UIManager.cs
+++ Assets/Scripts/Managers/Core/UIManager.cs
@@ -5,4 +5,48 @@ using UnityEngine;
 public class UIManager
 {
     int m_order = 0;
+
+    Stack<UI_Popup> m_popupStack = new();
+
+    public T ShowPopupUI<T>(string name = null) where T : UI_Popup
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Popup/{name}");
+        T popup = Util.GetOrAddComponent<T>(go);
+        m_popupStack.Push(popup);
+        return popup;
+    }
+
+    public void ClosePopupUI(UI_Popup popup)
+    {
+        if (m_popupStack.Count == 0)
+            return;
+
+        if (m_popupStack.Peek() != popup)
+        {
+            Debug.Log("Close Popup Failed");
+            return;
+        }
+
+        ClosePopupUI();
+    }
+
+    public void ClosePopupUI()
+    {
+        if (m_popupStack.Count == 0)
+            return;
+
+        UI_Popup popup = m_popupStack.Pop();
+        Managers.Resource.Destroy(popup.gameObject);
+        popup = null;
+        m_order--;
+    }
+
+    public void CloseAllPopupUI()
+    {
+        while (m_popupStack.Count > 0)
+            ClosePopupUI();
+    }
 }
