0fcf394c0fc5c3b42354055fcde9f35f594f251c
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-07 19:47:25 +0900

    make UI_Base & inheritance

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> m_objects = new();
 
     enum Buttons
     {
@@ -27,34 +26,5 @@ public class UI_Button : MonoBehaviour
         Bind<GameObject>(typeof(GameObjects));
     }
 
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        m_objects.Add(typeof(T), objects);
 
-        for (int i = 0; i < names.Length; i++)
-        {
-            if (typeof(T) == typeof(GameObject))
-                objects[i] = Util.FindChild(gameObject, names[i], true);
-            else
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-
-            if (objects[i] == null)
-                Debug.Log($"Failed to Bind: {names[i]}");
-        }
-    }
-
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (m_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
-
-        return objects[idx] as T;
-    }
-
-    Button GetButton(int idx) { return Get<Button>(idx); }
-    Text GetText(int idx) { return Get<Text>(idx); }
-    Image GetImage(int idx) { return Get<Image>(idx); }
 }
