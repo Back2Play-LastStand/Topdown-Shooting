1ec6afea0c22548595a0ef861b956f57b62cbf7f
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-07 18:08:09 +0900

    create UI_Button

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Button : MonoBehaviour
+{
+    enum Buttons
+    {
+    }
+
+    enum Texts
+    {
+    }
+
+    private void Start()
+    {
+        Bind();
+    }
+
+    void Bind()
+    {
+    }
+}
