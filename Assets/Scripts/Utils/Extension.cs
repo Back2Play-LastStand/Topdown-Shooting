9066c25eca1c9e713426c7d6fe50f57b7d9fa5ea
hyuli0520 <minhyeogjeon945@gmail.com>
2025-04-08 13:49:56 +0900

    update Extension
    make GetOrAddComponent

--- Assets/Scripts/Utils/Extension.cs
+++ Assets/Scripts/Utils/Extension.cs
@@ -6,6 +6,11 @@ using UnityEngine.EventSystems;
 
 public static class Extension
 {
+    public static T GetOrAddComponent<T>(this GameObject go) where T : UnityEngine.Component
+    {
+        return Util.GetOrAddComponent<T>(go);
+    }
+
     public static void AddUIEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
     {
         UI_Base.AddUIEvent(go, action, type);
